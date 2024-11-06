2c8c3f184af87b3bf6ced197f3d6df84ce0e7992
srg71 <r.samoilov@zonatelecom.ru>
2024-11-06 13:40:03 +0300

    configurator.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pprint
-
 from configurator import Configurator
 
-creds_path = 'files/secret/credentials.json'
+creds_path = 'files/secret/creds.json'
 local_start = True
+
 
 if __name__ == '__main__':
     config = Configurator(creds_path, local_start).get_config()
