b4046ca6b64e99c2919103ea758d33fe3bbc140f
srg71 <r.samoilov@zonatelecom.ru>
2024-11-06 14:02:07 +0300

    configurator.py

--- configurator.py
+++ configurator.py
@@ -2,7 +2,46 @@ from addition.api import *
 from addition.other import *
 
 
-class Configurator:
+class ConfiguratorAnaliser:
+    def __init__(self, creds_path, local_start=False):
+        self.creds_path = creds_path
+        self.save_path = 'files/config/config.xlsx'
+        self.sheet_names = ['Подключения', 'Таймфреймы', 'Эмулятор']
+        self.creds = json_to_dict(self.creds_path)
+
+        if not local_start:
+            self.save_config()
+        self.default_config = read_excel_file(self.save_path, self.sheet_names)
+
+    def save_config(self):
+        service_account_path = self.creds['config']['path']
+        file_link = self.creds['config']['link']
+
+        init = GoogleDriveAPI(service_account_path)
+        fileid = init.get_file_id(file_link)
+        init.download_file(real_file_id=fileid, file_path=self.save_path)
+        return self.save_path
+
+    def prepare_connection(self):
+        return False
+
+    def prepare_timeframe(self):
+        return False
+
+    def prepare_emulator(self):
+        return False
+
+    def get_config(self):
+        config = {
+            'credential': self.creds,
+            'connection': self.prepare_connection(),
+            'timeframe': self.prepare_timeframe(),
+            'emulator': self.prepare_emulator()
+        }
+        return config
+
+
+class ConfiguratorTelegram:
     def __init__(self, creds_path, local_start=False):
         self.creds_path = creds_path
         self.save_path = 'files/config/config.xlsx'
--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+from configurator import ConfiguratorAnaliser, ConfiguratorTelegram
 import pprint
-from configurator import Configurator
 
+# Параметры для запуска
 creds_path = 'files/secret/creds.json'
 local_start = True
 
+start_telegram = False
+start_clean_data_loader = True
+start_pa_analiser = False
+start_order_tester = False
+start_trader = False
+
 
 if __name__ == '__main__':
-    config = Configurator(creds_path, local_start).get_config()
-    pprint.pprint(config)
+    # Получение файлов конфигурации
+    config_anal = ConfiguratorAnaliser(creds_path, local_start).get_config()
+    pprint.pprint(config_anal)
+
+    # Запуск телеграм бота
+    if start_telegram:
+        config_telegram = ConfiguratorTelegram(creds_path, local_start).get_config()
+        pass
+
+    # Запуск модуля получения данных
+    if start_clean_data_loader:
+        pass
+
+    # Запуск модуля PriceAction (паттерны, индикаторы, уровни, тренды)
+    if start_pa_analiser:
+        pass
+
+    # Запуск модуля тестирования стратегий
+    if start_order_tester:
+        pass
+
+    # Запуск модуля торговли
+    if start_trader:
+        pass
