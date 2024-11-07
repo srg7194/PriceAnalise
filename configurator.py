4bb1aaf3c700feb1d51c22826502755a7bbf07ae
srg71 <r.samoilov@zonatelecom.ru>
2024-11-07 22:39:54 +0300

    configurator.py

--- configurator.py
+++ configurator.py
@@ -1,36 +1,51 @@
-from pprint import ппринт
 from addition.api import *
 from addition.other import *
 
+class Config:
+    def __init__(self, creds_path, local_start):
+        self.creds_path = creds_path
+        self.save_path_xslx = 'files/config/config.xlsx'
+        self.save_path_json = 'files/config/config.json'
+        self.creds = json_to_dict(self.creds_path)
+
+        if not local_start:
+            self.save_config()
+        self.default_config = read_excel_file(self.save_path_xslx)
+
+    def save_config(self):
+        service_account_path = self.creds['config']['path']
+        file_link = self.creds['config']['link']
+
+        init = GoogleDriveAPI(service_account_path)
+        fileid = init.get_file_id(file_link)
+        init.download_file(real_file_id=fileid, file_path=self.save_path_xslx)
+        return self.save_path_xslx
+
+    def get_config(self):
+        config = {
+            'credential': self.creds,
+            'loader': Loader(self.default_config).get_config()
+        }
+        dict_to_json(config, self.save_path_json)
+        return config
+
 
 class Loader:
-    def __init__(self, creds_path, local_start=False):
-        self.creds_path = creds_path
-        self.save_path_xslx = 'files/config/loader.xlsx'
-        self.save_path_json = 'files/config/loader.json'
-        self.creds = json_to_dict(self.creds_path)
+    def __init__(self, default_config):
+        self.default_config = default_config
 
-        if not local_start:
-            self.save_config()
-        self.default_config = read_excel_file(self.save_path_xslx)
-
-    def save_config(self):
-        service_account_path = self.creds['config']['path']
-        file_link = self.creds['config']['link']
-
-        init = GoogleDriveAPI(service_account_path)
-        fileid = init.get_file_id(file_link)
-        init.download_file(real_file_id=fileid, file_path=self.save_path_xslx)
-        return self.save_path_xslx
-
-    def get_data_loader(self):
-        # Фильтры
+    def filter_enabled(self):
         connection = self.default_config['connection']
         enable_connection = connection.loc[connection['enable'] == True]
+        return enable_connection
 
+    def update_data(self):
         timeframe = self.default_config['timeframe']
         timeframe = timeframe.map(lambda x: None if pd.isna(x) else x)
+        return timeframe
 
+    @staticmethod
+    def get_data_loader(enable_connection, timeframe):
         config = []
         for i, s in enable_connection.iterrows():
             temp = dict(s)
@@ -44,206 +59,8 @@ class Loader:
         return config
 
     def get_config(self):
-        config = {
-            'credential': self.creds,
-            'loader': self.get_data_loader()
-        }
-        dict_to_json(config, self.save_path_json)
-        return config
-
-
-class Analiser:
-    def __init__(self, creds_path, local_start=False):
-        self.creds_path = creds_path
-        self.save_path_xslx = 'files/config/analiser.xlsx'
-        self.save_path_json = 'files/config/analiser.json'
-        self.creds = json_to_dict(self.creds_path)
-
-        if not local_start:
-            self.save_config()
-        self.default_config = read_excel_file(self.save_path_xslx)
-
-    def save_config(self):
-        service_account_path = self.creds['config']['path']
-        file_link = self.creds['config']['link']
-
-        init = GoogleDriveAPI(service_account_path)
-        fileid = init.get_file_id(file_link)
-        init.download_file(real_file_id=fileid, file_path=self.save_path_xslx)
-        return self.save_path_xslx
-
-    def get_data_loader(self):
-        # Фильтры
-        connection = self.default_config['connection']
-        enable_connection = connection.loc[connection['enable'] == True]
-
-        timeframe = self.default_config['timeframe']
-        timeframe = timeframe.map(lambda x: None if pd.isna(x) else x)
-
-        config = []
-        for i, s in enable_connection.iterrows():
-            temp = dict(s)
-            for el in ['enable', 'id']:
-                temp.pop(el)
-            enable_timeframe = (timeframe.loc[timeframe['Id_connection'] == s['id']])
-            enable_timeframe.pop('Id_connection')
-            enable_timeframe = enable_timeframe.to_dict('records')
-            temp.update({'timeframe': enable_timeframe})
-            config.append(temp)
-        return config
-
-    def get_config(self):
-        config = {
-            'credential': self.creds,
-            'loader': self.get_data_loader()
-        }
-        dict_to_json(config, self.save_path_json)
-        return config
-
-
-class Emulator:
-    def __init__(self, creds_path, local_start=False):
-        self.creds_path = creds_path
-        self.save_path_xslx = 'files/config/loader.xlsx'
-        self.save_path_json = 'files/config/loader.json'
-        self.creds = json_to_dict(self.creds_path)
-
-        if not local_start:
-            self.save_config()
-        self.default_config = read_excel_file(self.save_path_xslx)
-
-    def save_config(self):
-        service_account_path = self.creds['config']['path']
-        file_link = self.creds['config']['link']
-
-        init = GoogleDriveAPI(service_account_path)
-        fileid = init.get_file_id(file_link)
-        init.download_file(real_file_id=fileid, file_path=self.save_path_xslx)
-        return self.save_path_xslx
-
-    def get_data_loader(self):
-        # Фильтры
-        connection = self.default_config['connection']
-        enable_connection = connection.loc[connection['enable'] == True]
-
-        timeframe = self.default_config['timeframe']
-        timeframe = timeframe.map(lambda x: None if pd.isna(x) else x)
-
-        config = []
-        for i, s in enable_connection.iterrows():
-            temp = dict(s)
-            for el in ['enable', 'id']:
-                temp.pop(el)
-            enable_timeframe = (timeframe.loc[timeframe['Id_connection'] == s['id']])
-            enable_timeframe.pop('Id_connection')
-            enable_timeframe = enable_timeframe.to_dict('records')
-            temp.update({'timeframe': enable_timeframe})
-            config.append(temp)
-        return config
-
-    def get_config(self):
-        config = {
-            'credential': self.creds,
-            'loader': self.get_data_loader()
-        }
-        dict_to_json(config, self.save_path_json)
-        return config
-
-
-class Trader:
-    def __init__(self, creds_path, local_start=False):
-        self.creds_path = creds_path
-        self.save_path_xslx = 'files/config/loader.xlsx'
-        self.save_path_json = 'files/config/loader.json'
-        self.creds = json_to_dict(self.creds_path)
-
-        if not local_start:
-            self.save_config()
-        self.default_config = read_excel_file(self.save_path_xslx)
-
-    def save_config(self):
-        service_account_path = self.creds['config']['path']
-        file_link = self.creds['config']['link']
-
-        init = GoogleDriveAPI(service_account_path)
-        fileid = init.get_file_id(file_link)
-        init.download_file(real_file_id=fileid, file_path=self.save_path_xslx)
-        return self.save_path_xslx
-
-    def get_data_loader(self):
-        # Фильтры
-        connection = self.default_config['connection']
-        enable_connection = connection.loc[connection['enable'] == True]
-
-        timeframe = self.default_config['timeframe']
-        timeframe = timeframe.map(lambda x: None if pd.isna(x) else x)
-
-        config = []
-        for i, s in enable_connection.iterrows():
-            temp = dict(s)
-            for el in ['enable', 'id']:
-                temp.pop(el)
-            enable_timeframe = (timeframe.loc[timeframe['Id_connection'] == s['id']])
-            enable_timeframe.pop('Id_connection')
-            enable_timeframe = enable_timeframe.to_dict('records')
-            temp.update({'timeframe': enable_timeframe})
-            config.append(temp)
-        return config
-
-    def get_config(self):
-        config = {
-            'credential': self.creds,
-            'loader': self.get_data_loader()
-        }
-        dict_to_json(config, self.save_path_json)
-        return config
-
-
-class Telegram:
-    def __init__(self, creds_path, local_start=False):
-        self.creds_path = creds_path
-        self.save_path_xslx = 'files/config/loader.xlsx'
-        self.save_path_json = 'files/config/loader.json'
-        self.creds = json_to_dict(self.creds_path)
-
-        if not local_start:
-            self.save_config()
-        self.default_config = read_excel_file(self.save_path_xslx)
-
-    def save_config(self):
-        service_account_path = self.creds['config']['path']
-        file_link = self.creds['config']['link']
-
-        init = GoogleDriveAPI(service_account_path)
-        fileid = init.get_file_id(file_link)
-        init.download_file(real_file_id=fileid, file_path=self.save_path_xslx)
-        return self.save_path_xslx
-
-    def get_data_loader(self):
-        # Фильтры
-        connection = self.default_config['connection']
-        enable_connection = connection.loc[connection['enable'] == True]
-
-        timeframe = self.default_config['timeframe']
-        timeframe = timeframe.map(lambda x: None if pd.isna(x) else x)
-
-        config = []
-        for i, s in enable_connection.iterrows():
-            temp = dict(s)
-            for el in ['enable', 'id']:
-                temp.pop(el)
-            enable_timeframe = (timeframe.loc[timeframe['Id_connection'] == s['id']])
-            enable_timeframe.pop('Id_connection')
-            enable_timeframe = enable_timeframe.to_dict('records')
-            temp.update({'timeframe': enable_timeframe})
-            config.append(temp)
-        return config
-
-    def get_config(self):
-        config = {
-            'credential': self.creds,
-            'loader': self.get_data_loader()
-        }
-        dict_to_json(config, self.save_path_json)
+        enable_connection = self.filter_enabled()
+        timeframe = self.update_data()
+        config = self.get_data_loader(enable_connection, timeframe)
         return config
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from configurator import Loader, ConfiguratorTelegram
-import pprint
+from configurator import Config
 
 # Параметры для запуска
 creds_path = 'files/secret/creds.json'
@@ -12,28 +11,9 @@ start_order_tester = False
 start_trader = False
 
 
+
+
+
 if __name__ == '__main__':
     # Получение файлов конфигурации
-    config_anal = Loader(creds_path, local_start).get_config()
-    pprint.pprint(config_anal, sort_dicts=False)
-
-    # Запуск телеграм бота
-    if start_telegram:
-        config_telegram = ConfiguratorTelegram(creds_path, local_start).get_config()
-        pass
-
-    # Запуск модуля получения данных
-    if start_clean_data_loader:
-        pass
-
-    # Запуск модуля PriceAction (паттерны, индикаторы, уровни, тренды)
-    if start_pa_analiser:
-        pass
-
-    # Запуск модуля тестирования стратегий
-    if start_order_tester:
-        pass
-
-    # Запуск модуля торговли
-    if start_trader:
-        pass
+    config = Config(creds_path, local_start).get_config()
